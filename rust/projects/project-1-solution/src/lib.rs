fe2b89d6fca6d554a1007600b8cc548e67495c44
cedrickchee <cedrick.chee@gmail.com>
2019-11-17 04:40:27 +0800

    Make the tests compile
    This is the work related to this part 1 of Project 1:
    https://github.com/pingcap/talent-plan/blob/master/rust/projects/project-1/project.md#part-1-make-the-tests-compile
    
    Currently, tests can compile (but will still not run and pass).

--- rust/projects/project-1-solution/src/lib.rs
+++ rust/projects/project-1-solution/src/lib.rs
@@ -1,1 +1,19 @@
+pub struct KvStore {}
 
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {}
+    }
+
+    pub fn set(&self, _key: String, _value: String) {
+        unimplemented!();
+    }
+
+    pub fn get(&self, _key: String) -> Option<String> {
+        unimplemented!();
+    }
+
+    pub fn remove(&self, _key: String) {
+        unimplemented!();
+    }
+}
