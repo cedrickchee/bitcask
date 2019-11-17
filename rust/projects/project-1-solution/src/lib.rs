47ed72be4f6360b1b45a30652967c78dd97a00a9
cedrickchee <cedrick.chee@gmail.com>
2019-11-18 01:22:49 +0800

    Refactor lib
    Break lib into a new kv module & re-export module.

--- rust/projects/project-1-solution/src/lib.rs
+++ rust/projects/project-1-solution/src/lib.rs
@@ -3,74 +3,7 @@
 //! An in-memory key/value store module
 
 #![deny(missing_docs)]
-use std::collections::HashMap;
 
-/// A key-value store of String keys and values.
-pub struct KvStore {
-    storage: HashMap<String, String>,
-}
+mod kv;
 
-impl KvStore {
-    /// Constructs a new `KvStore`.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use kvs::KvStore;
-    ///
-    /// let mut store = KvStore::new();
-    /// ```
-    pub fn new() -> Self {
-        Self {
-            storage: HashMap::new(),
-        }
-    }
-
-    /// Set a given key and value Strings in the store.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use kvs::KvStore;
-    ///
-    /// let mut store = KvStore::new();
-    /// store.set(String::from("my_key"), String::from("my_value"));
-    /// ```
-    pub fn set(&mut self, key: String, value: String) {
-        self.storage.insert(key, value);
-    }
-
-    /// Get a value from the store using a key String.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use kvs::KvStore;
-    ///
-    /// let store = KvStore::new();
-    /// match store.get(String::from("my_key")) {
-    ///     Some(value) => println!("Value: {}", value),
-    ///     None => println!("Key not found"),
-    /// }
-    /// ```
-    pub fn get(&self, key: String) -> Option<String> {
-        match self.storage.get(&key) {
-            Some(value) => Some(value.clone()),
-            None => None,
-        }
-    }
-
-    /// Remove a given key from the store.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use kvs::KvStore;
-    ///
-    /// let mut store = KvStore::new();
-    /// store.remove(String::from("my_key"));
-    /// ```
-    pub fn remove(&mut self, key: String) {
-        self.storage.remove(&key);
-    }
-}
+pub use kv::KvStore;
