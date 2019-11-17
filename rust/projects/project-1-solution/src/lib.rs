b92f5643c417dc0a082ad07d8cfb00d444f878f0
cedrickchee <cedrick.chee@gmail.com>
2019-11-17 14:00:42 +0800

    Add documentation
    This completes part 5 of project 1:
    https://github.com/pingcap/talent-plan/blob/master/rust/projects/project-1/project.md#part-5-documentation
    
    This add documentation for the kvs library, and some examples
    that can be run with `cargo test --doc` for each public method.

--- rust/projects/project-1-solution/src/lib.rs
+++ rust/projects/project-1-solution/src/lib.rs
@@ -1,20 +1,58 @@
+//! # Kvs
+//!
+//! An in-memory key/value store module
+
+#[deny(missing_docs)]
 use std::collections::HashMap;
 
+/// A key-value store of String keys and values.
 pub struct KvStore {
     storage: HashMap<String, String>,
 }
 
 impl KvStore {
+    /// Constructs a new `KvStore`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// ```
     pub fn new() -> Self {
         Self {
             storage: HashMap::new(),
         }
     }
 
+    /// Set a given key and value Strings in the store.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// store.set(String::from("my_key"), String::from("my_value"));
+    /// ```
     pub fn set(&mut self, key: String, value: String) {
         self.storage.insert(key, value);
     }
 
+    /// Get a value from the store using a key String.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let store = KvStore::new();
+    /// match store.get(String::from("my_key")) {
+    ///     Some(value) => println!("Value: {}", value),
+    ///     None => println!("Key not found"),
+    /// }
+    /// ```
     pub fn get(&self, key: String) -> Option<String> {
         match self.storage.get(&key) {
             Some(value) => Some(value.clone()),
@@ -22,6 +60,16 @@ impl KvStore {
         }
     }
 
+    /// Remove a given key from the store.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// store.remove(String::from("my_key"));
+    /// ```
     pub fn remove(&mut self, key: String) {
         self.storage.remove(&key);
     }
