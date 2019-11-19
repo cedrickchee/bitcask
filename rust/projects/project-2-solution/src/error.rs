a34eda72e78528aa1ea6d9d73548a8b395912d32
cedrickchee <cedrick.chee@gmail.com>
2019-11-19 21:54:11 +0800

    Update comments of error type

--- rust/projects/project-2-solution/src/error.rs
+++ rust/projects/project-2-solution/src/error.rs
@@ -4,14 +4,23 @@ use std::io;
 /// Error type. It represents the ways a kvs could be invalid.
 #[derive(Fail, Debug)]
 pub enum KvsError {
-    /// An IO error.
+    /// An IO error. Wraps a `std::io::Error`.
     #[fail(display = "{}", _0)]
     Io(#[fail(cause)] io::Error),
+    /// Serialization or deserialization error.
+    #[fail(display = "{}", _0)]
+    Serde(#[fail(cause)] serde_json::Error),
 }
 
 impl From<io::Error> for KvsError {
     fn from(error: io::Error) -> Self {
         Self::Io(error)
+    }
+}
+
+impl From<serde_json::Error> for KvsError {
+    fn from(error: serde_json::Error) -> Self {
+        Self::Serde(error)
     }
 }
 
