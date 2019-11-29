899583c068c61f7fe5c39b078e182bca5d626a4c
cedrickchee <cedrick.chee@gmail.com>
2019-11-29 09:16:38 +0800

    Refactor: simplify KvsClient, clean code

--- rust/projects/project-5-solution/src/client.rs
+++ rust/projects/project-5-solution/src/client.rs
@@ -1,6 +1,7 @@
 use std::net::SocketAddr;
 
 use tokio::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+use tokio::io::{ReadHalf, WriteHalf};
 use tokio::net::TcpStream;
 use tokio::prelude::*;
 use tokio_serde_json::{ReadJson, WriteJson};
@@ -10,86 +11,80 @@ use crate::KvsError;
 
 /// The client of a key value store.
 pub struct KvsClient {
-    tcp: Option<TcpStream>,
+    read_json: ReadJson<FramedRead<ReadHalf<TcpStream>, LengthDelimitedCodec>, Response>,
+    write_json: WriteJson<FramedWrite<WriteHalf<TcpStream>, LengthDelimitedCodec>, Request>,
 }
 
 impl KvsClient {
     /// Connect to `addr` to access `KvsServer`.
     pub fn connect(addr: SocketAddr) -> impl Future<Item = Self, Error = KvsError> {
         TcpStream::connect(&addr)
-            .map(|tcp| KvsClient { tcp: Some(tcp) })
+            .map(|tcp| {
+                let (read_half, write_half) = tcp.split();
+                let read_json =
+                    ReadJson::new(FramedRead::new(read_half, LengthDelimitedCodec::new()));
+                let write_json =
+                    WriteJson::new(FramedWrite::new(write_half, LengthDelimitedCodec::new()));
+                KvsClient {
+                    read_json,
+                    write_json,
+                }
+            })
             .map_err(|e| e.into())
     }
 
     /// Get a value from the server using a key String.
-    pub fn get(
-        mut self,
-        key: String,
-    ) -> impl Future<Item = (Option<String>, Self), Error = KvsError> {
-        let tcp = self.tcp.take().unwrap();
-        let write_json = WriteJson::new(FramedWrite::new(tcp, LengthDelimitedCodec::new()));
-        let tcp = write_json
-            .send(Request::Get { key })
-            .map(|serialized| serialized.into_inner().into_inner());
-        tcp.and_then(|tcp| {
-            let read_json = ReadJson::new(FramedRead::new(tcp, LengthDelimitedCodec::new()));
-            read_json.into_future().map_err(|(err, _)| err)
-        })
-        .map_err(|e| e.into())
-        .and_then(move |(resp, read_json)| {
-            self.tcp = Some(read_json.into_inner().into_inner());
-            match resp {
-                Some(Response::Get(value)) => Ok((value, self)),
+    pub fn get(self, key: String) -> impl Future<Item = (Option<String>, Self), Error = KvsError> {
+        self.send_request(Request::Get { key })
+            .and_then(move |(resp, client)| match resp {
+                Some(Response::Get(value)) => Ok((value, client)),
                 Some(Response::Err(msg)) => Err(KvsError::StringError(msg)),
                 Some(_) => Err(KvsError::StringError("Invalid response".to_owned())),
                 None => Err(KvsError::StringError("No response received".to_owned())),
-            }
-        })
+            })
     }
 
     /// Set a given key and value Strings in the server.
-    pub fn set(mut self, key: String, value: String) -> impl Future<Item = Self, Error = KvsError> {
-        let tcp = self.tcp.take().unwrap();
-        let write_json = WriteJson::new(FramedWrite::new(tcp, LengthDelimitedCodec::new()));
-        let tcp = write_json
-            .send(Request::Set { key, value })
-            .map(|serialized| serialized.into_inner().into_inner());
-        tcp.and_then(|tcp| {
-            let read_json = ReadJson::new(FramedRead::new(tcp, LengthDelimitedCodec::new()));
-            read_json.into_future().map_err(|(err, _)| err)
-        })
-        .map_err(|e| e.into())
-        .and_then(move |(resp, read_json)| {
-            self.tcp = Some(read_json.into_inner().into_inner());
-            match resp {
-                Some(Response::Set) => Ok(self),
+    pub fn set(self, key: String, value: String) -> impl Future<Item = Self, Error = KvsError> {
+        self.send_request(Request::Set { key, value })
+            .and_then(move |(resp, client)| match resp {
+                Some(Response::Set) => Ok(client),
                 Some(Response::Err(msg)) => Err(KvsError::StringError(msg)),
                 Some(_) => Err(KvsError::StringError("Invalid response".to_owned())),
                 None => Err(KvsError::StringError("No response received".to_owned())),
-            }
-        })
+            })
     }
 
     /// Remove a given key from the server.
-    pub fn remove(mut self, key: String) -> impl Future<Item = Self, Error = KvsError> {
-        let tcp = self.tcp.take().unwrap();
-        let write_json = WriteJson::new(FramedWrite::new(tcp, LengthDelimitedCodec::new()));
-        let tcp = write_json
-            .send(Request::Remove { key })
-            .map(|serialized| serialized.into_inner().into_inner());
-        tcp.and_then(|tcp| {
-            let read_json = ReadJson::new(FramedRead::new(tcp, LengthDelimitedCodec::new()));
-            read_json.into_future().map_err(|(err, _)| err)
-        })
-        .map_err(|e| e.into())
-        .and_then(move |(resp, read_json)| {
-            self.tcp = Some(read_json.into_inner().into_inner());
-            match resp {
-                Some(Response::Remove) => Ok(self),
+    pub fn remove(self, key: String) -> impl Future<Item = Self, Error = KvsError> {
+        self.send_request(Request::Remove { key })
+            .and_then(move |(resp, client)| match resp {
+                Some(Response::Remove) => Ok(client),
                 Some(Response::Err(msg)) => Err(KvsError::StringError(msg)),
                 Some(_) => Err(KvsError::StringError("Invalid response".to_owned())),
                 None => Err(KvsError::StringError("No response received".to_owned())),
-            }
-        })
+            })
+    }
+
+    fn send_request(
+        self,
+        req: Request,
+    ) -> impl Future<Item = (Option<Response>, Self), Error = KvsError> {
+        let read_json = self.read_json;
+        self.write_json
+            .send(req)
+            .and_then(move |write_json| {
+                read_json
+                    .into_future()
+                    .map(move |(resp, read_json)| {
+                        let client = KvsClient {
+                            read_json,
+                            write_json,
+                        };
+                        (resp, client)
+                    })
+                    .map_err(|(err, _)| err)
+            })
+            .map_err(|e| e.into())
     }
 }
